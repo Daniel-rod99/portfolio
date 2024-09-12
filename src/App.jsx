dd677ddb4f143ec3792055ede574f163f3f22687
Daniel-rod99 <danielrodriguez246@gmail.com>
2024-09-12 15:15:56 +0200

    Update Project Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./Global.css";
 import Header from "./Components/header";
 import Home from "./Home";
+import ParticlesBackground from "./Components/ParticlesBackground";
 
 function App() {
   return (
     <>
+      <div className="absolute">
+        <ParticlesBackground />
+      </div>
       <Header />
       <Home />
     </>
