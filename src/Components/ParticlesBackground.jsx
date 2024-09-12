4f3b6e158493c9754eb06f257e9c44719a48b11d
Daniel-rod99 <danielrodriguez246@gmail.com>
2024-09-12 15:16:45 +0200

    Update Project Portfolio

--- src/Components/ParticlesBackground.jsx
+++ src/Components/ParticlesBackground.jsx
@@ -68,7 +68,7 @@ const ParticlesBackground = () => {
                 default: "bounce",
               },
               random: false,
-              speed: 6,
+              speed: 2,
               straight: false,
             },
             number: {
